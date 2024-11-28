6a7dd0a76e713e5c763d98b196734fdc7ba976f6
Gaurav Bhattacharjee <144083709+0xgh057r3c0n@users.noreply.github.com>
2024-11-28 15:39:49 +0530

    Update DorkScanner.py

--- DorkScanner.py
+++ DorkScanner.py
@@ -1,43 +1,34 @@
-from googlesearch import search  # Ensure this is the correct import
+from googlesearch import search
+from colorama import Fore, Style, init
+init(autoreset=True)
 
-# Banner Information
-BANNER = """
-ooo.                8      .oPYo.                                        
-8  `8.              8      8                                             
-8   `8 .oPYo. oPYo. 8  .o  `Yooo. .oPYo. .oPYo. odYo. odYo. .oPYo. oPYo. 
-8    8 8    8 8  `' 8oP'       `8 8    ' .oooo8 8' `8 8' `8 8oooo8 8  `' 
-8   .P 8    8 8     8 `b.       8 8    . 8    8 8   8 8   8 8.     8     
-8ooo'  `YooP' 8     8  `o. `YooP' `YooP' `YooP8 8   8 8   8 `Yooo' 8     
-.....:::.....:..::::..::...:.....::.....::.....:..::....::..:.....:..::::
-                       Google Dorking Tool
-                          Version: 1.0
-                       Author: G4UR4V007
-.....:::.....:..::::..::...:.....::.....::.....:..::....::..:.....:..::::
+BANNER = f"""
+{Fore.CYAN}ooo.                8      .oPYo.                                        
+{Fore.CYAN}8  `8.              8      8                                             
+{Fore.CYAN}8   `8 {Fore.RED}.oPYo.{Fore.CYAN} oPYo. 8  .o  {Fore.YELLOW}`Yooo.{Fore.CYAN} .oPYo. .oPYo. {Fore.GREEN}odYo. odYo.{Fore.CYAN} .oPYo. oPYo. 
+{Fore.CYAN}8    8 {Fore.RED}8    8{Fore.CYAN} 8  `' 8oP'       {Fore.YELLOW}`8{Fore.CYAN} 8    ' .oooo8 {Fore.GREEN}8' `8 8' `8{Fore.CYAN} 8oooo8 8  `' 
+{Fore.CYAN}8   .P {Fore.RED}8    8{Fore.CYAN} 8     8 `b.       {Fore.YELLOW}8{Fore.CYAN} 8    . 8    8 {Fore.GREEN}8   8 8   8{Fore.CYAN} 8.     8     
+{Fore.CYAN}8ooo'  {Fore.RED}`YooP'{Fore.CYAN} 8     8  `o. {Fore.YELLOW}`YooP'{Fore.CYAN} `YooP' `YooP8 {Fore.GREEN}8   8 8   8{Fore.CYAN} `Yooo' 8     
+{Fore.CYAN}.....:::.....:..::::..::...:.....::.....::.....:..::....::..:.....:..::::
+                       {Fore.MAGENTA}Google Dorking Tool
+                          {Fore.MAGENTA}Version: 1.0
+                       {Fore.MAGENTA}Author: G4UR4V007
+{Fore.CYAN}.....:::.....:..::::..::...:.....::.....::.....:..::....::..:.....:..::::
 """
 
 def google_dork(query, num_results=10):
-    """
-    Perform a Google search using the given dorking query.
-
-    Args:
-        query (str): The dorking query to use.
-        num_results (int): The number of results to return. Defaults to 10.
-
-    Returns:
-        list: A list of search results.
-    """
-    results = list(search(query, num_results=num_results))  # Use the correct search function
+    results = list(search(query, num_results=num_results))
     return results
 
 def main():
     print(BANNER)
-    query = input("Enter a dorking query: ")
-    num_results = int(input("Enter the number of results to return (default=10): ") or 10)
+    query = input(f"{Fore.GREEN}Enter a dorking query: {Style.RESET_ALL}")
+    num_results = int(input(f"{Fore.GREEN}Enter the number of results to return (default=10): {Style.RESET_ALL}") or 10)
     results = google_dork(query, num_results)
 
-    print("\nSearch Results:")
-    for result in results:
-        print(result)  # Print each result directly
+    print(f"\n{Fore.YELLOW}Search Results:")
+    for i, result in enumerate(results, 1):
+        print(f"{Fore.CYAN}{i}. {Fore.BLUE}{result}")
 
 if __name__ == "__main__":
     main()
